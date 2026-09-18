96dd8066ba8f0b1cec7c7169aa4e0705e0d15912
Botsiture <bismillaha443@gmail.com>
2026-09-18 07:40:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ API_KEY = getenv("API_KEY", None) # Get This API KEY FROM OWNER: @SpYtAPIBot
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DevloperSP/MusicSp",
+    "https://t.me/iMSASUKESi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Anime_Group_hai") 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Sand_Village")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Sand_Village") 
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Anime_Group_hai")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
