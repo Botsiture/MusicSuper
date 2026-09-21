7be5c73e0149e7f6a5e8ecd2e20ab46f581cc6dd
Botsiture <bismillaha443@gmail.com>
2026-09-22 00:21:49 +0530

    Update resume.py

--- MusicSp/plugins/admins/resume.py
+++ MusicSp/plugins/admins/resume.py
@@ -5,17 +5,30 @@ from MusicSp import app
 from MusicSp.core.call import DevSp
 from MusicSp.utils.database import is_music_playing, music_on
 from MusicSp.utils.decorators import AdminRightsCheck
-from MusicSp.utils.inline import close_markup
+from MusicSp.utils.inline import close_markup, refresh_player_markup
 from config import BANNED_USERS
 
-
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["resume", "cresume"])
+    & filters.group
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
+
     await music_on(chat_id)
     await DevSp.resume_stream(chat_id)
+
+    # Change player button from Resume -> Pause dynamically
+    await refresh_player_markup(
+        _,
+        chat_id,
+        playing=True,
+    )
+
     await message.reply_text(
-        _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_4"].format(message.from_user.mention),
+        reply_markup=close_markup(_),
     )
