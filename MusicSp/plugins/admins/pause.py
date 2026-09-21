f48bdac3fe4178a29e847fa1ff4c648f260b9ec9
Botsiture <bismillaha443@gmail.com>
2026-09-22 00:21:16 +0530

    Update pause.py

--- MusicSp/plugins/admins/pause.py
+++ MusicSp/plugins/admins/pause.py
@@ -5,17 +5,30 @@ from MusicSp import app
 from MusicSp.core.call import DevSp
 from MusicSp.utils.database import is_music_playing, music_off
 from MusicSp.utils.decorators import AdminRightsCheck
-from MusicSp.utils.inline import close_markup
+from MusicSp.utils.inline import close_markup, refresh_player_markup
 from config import BANNED_USERS
 
-
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["pause", "cpause"])
+    & filters.group
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
+
     await music_off(chat_id)
     await DevSp.pause_stream(chat_id)
+
+    # Change player button from Pause -> Resume dynamically
+    await refresh_player_markup(
+        _,
+        chat_id,
+        playing=False,
+    )
+
     await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_2"].format(message.from_user.mention),
+        reply_markup=close_markup(_),
     )
